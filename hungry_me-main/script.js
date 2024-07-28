cc629ea1300201109da137d59340f02ca51dc937
sandeep-garai <79070318+sandeep-garai@users.noreply.github.com>
2024-07-28 19:13:38 +0530

    first commit

---
+++ hungry_me-main/script.js
@@ -0,0 +1,42 @@
+const navbarMenu = document.querySelector(".navbar .links")
+const showPopupBtn = document.querySelector(".login-btn");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const menuBtn = document.querySelector(".menu-btn");
+const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const formPopup = document.querySelector(".form-popup");
+const loginSignUp = document.querySelectorAll(".form-box .bottom-link a");
+const showContent=document.querySelector(".content");
+const showContentBtn=document.querySelector(".content button");
+
+//menu button
+menuBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
+    showContent.classList.toggle("show-content");
+})
+//hide menu
+hideMenuBtn.addEventListener("click", () => {
+    menuBtn.click()
+    
+});
+
+//show button
+showPopupBtn.addEventListener("click", () =>{
+    document.body.classList.toggle("show-popup");
+});
+//hide button
+hidePopupBtn.addEventListener("click",()=> {
+    showPopupBtn.click();
+});
+
+loginSignUp.forEach(link => {
+    link.addEventListener("click",(e) => {
+        e.preventDefault();
+        formPopup.classList[link.id==="signup-link" ? 'add' : 'remove']("show-signup");
+    });
+});
+
+//show content
+showContentBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show-popup");
+});
+
